fd793317d54c0c2aa86c3ee16ef0c2833223fd13
Phil Stainer <philstainer.dev@gmail.com>
2022-10-04 08:49:23 +0100

    update

--- .config/nvim/lua/phil/lsp/servers/sumneko_lua.lua
+++ .config/nvim/lua/phil/lsp/servers/sumneko_lua.lua
@@ -2,15 +2,15 @@ return function()
   return {
     on_attach = function(client, bufnr)
       if client.name == 'sumneko_lua' then
-        client.resolved_capabilities.document_formatting = false
+        client.server_capabilities.document_formatting = false
       end
     end,
     settings = {
       Lua = {
         diagnostics = {
           -- Get the language server to recognize the `vim` global
-          globals = {'vim'},
-        },
+          globals = { 'vim' }
+        }
       }
     }
   }
--- .config/nvim/lua/phil/lsp/servers/tsserver.lua
+++ .config/nvim/lua/phil/lsp/servers/tsserver.lua
@@ -4,7 +4,7 @@ return function()
   return {
     on_attach = function(client, bufnr)
       -- tsserver, stop messing with prettier da fuck!
-      client.resolved_capabilities.document_formatting = false
+      client.server_capabilities.document_formatting = false
 
       remaps.set_typescript(client, bufnr)
 
